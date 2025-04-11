f3e6db379e58fdef1216ac340ddec067ccbe824e
kelemnati <natikeleme1@gmail.com>
2025-04-11 10:46:58 +0300

    auth implemented for user part

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const dbConnect = require("./config/db_connect");
 const adminAuthRoute = require("./routes/admin/adminAuthRoutes");
+const userAuthRoutes = require("./routes/user/userAuthRoutes");
 
 dbConnect();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/v1/admin/auth", adminAuthRoute);
+app.use("/api/v1/user/auth", userAuthRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
